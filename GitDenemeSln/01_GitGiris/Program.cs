f1292535284059ca018475df320a8f926a60be15
Ekrem Öztürk <ekrem.ozturk03@gmail.com>
2021-01-31 18:06:22 +0300

    Yeni satır eklendi

--- GitDenemeSln/01_GitGiris/Program.cs
+++ GitDenemeSln/01_GitGiris/Program.cs
@@ -11,6 +11,7 @@ namespace _01_GitGiris
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Github");
+            Console.WriteLine("Yeni Satır");
 
             Console.ReadKey();
         }
